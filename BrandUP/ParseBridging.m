f772091fa40eed6540d3a9cb7b6738a726ae1202
ShawnCCHuang <shawn@blackspears.com>
2015-01-19 15:16:19 +0800

    set up app screen size

---
+++ BrandUP/ParseBridging.m
@@ -0,0 +1,13 @@
+//
+//  ParseBridging.m
+//  BrandUP
+//
+//  Created by ShawnCCHuang on 2015-01-19.
+//  Copyright (c) 2015 ShawnCCHuang. All rights reserved.
+//
+
+#import "ParseBridging.h"
+
+@implementation ParseBridging
+
+@end
